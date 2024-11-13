1fe4ce7bbe2fbdfe520572709ab5d5231b3680f2
Amaral <dmrramaral@gmail.com>
2024-11-13 12:42:05 -0300

    refactor styles and enhance Mercado component; rename button class, add filter categories, and update dependencies

--- src/app/home/mercado/mercado.component.ts
+++ src/app/home/mercado/mercado.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MercadoService } from './mercado.service';
+import { FormsModule } from '@angular/forms';
+import { Popover } from 'bootstrap';
 import { Mercado } from '../../models/mercado';
+import { MercadoService } from './mercado.service';
 
 @Component({
   selector: 'app-mercado',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule
 
   ],
   templateUrl: './mercado.component.html',
@@ -16,6 +19,15 @@ import { Mercado } from '../../models/mercado';
 export class MercadoComponent {
 
   mercado: Mercado;
+  mercadoDnD: any;
+
+  filter = {
+    category: '',
+    subcategory: ''
+  }
+
+  categories: any[] = [];
+  subcategories: any[] = [];
 
 
 
@@ -23,10 +35,41 @@ export class MercadoComponent {
     this.mercado = { armas: [], comidas: [], bebidas: [], armaduras: [], aneis: [], amuletos: [] };
   }
 
+
+
   ngOnInit() {
     this.mercadoService.getMercado().subscribe((data: any) => {
       this.mercado = data.mercado as Mercado;
 
+    });
+
+   
+
+    this.mercadoService.getMercadoDnD().subscribe((data: any) => {
+      this.mercadoDnD = data;
+      this.categories = [...new Set(this.mercadoDnD
+        .filter((item: any) => item.equipment_category && item.equipment_category.name)
+        .map((item: any) => item.equipment_category.name as string)
+      )].sort((a, b) => (a as string).localeCompare(b as string));
+
+      this.subcategories = [...new Set(this.mercadoDnD
+        .filter((item: any) => item.gear_category && item.gear_category.name)
+        .map((item: any) => item.gear_category.name as string)
+      )].sort((a, b) => (a as string).localeCompare(b as string));
+    });
+
+    /* this.mercadoService.getCaracteristicasEquipamentos(); */
+  }
+
+  applyFilters() {
+    this.mercadoService.getMercadoDnD().subscribe((data: any) => {
+      this.mercadoDnD = data.filter((item: any) => {
+        const result = (this.filter.category ? item.equipment_category.name === this.filter.category : true) &&
+          (this.filter.subcategory ? item.gear_category && item.gear_category.name === this.filter.subcategory : true);
+
+
+        return result;
+      });
     });
   }
 
--- src/app/home/mercado/mercado.service.ts
+++ src/app/home/mercado/mercado.service.ts
@@ -8,11 +8,37 @@ import { take } from 'rxjs';
 export class MercadoService {
 
   private readonly API = '/assets/mercado.json';
+  private readonly APIMercado = '/assets/equipments.json';
+  private apiDnD = 'https://www.dnd5eapi.co/api/equipment';
   
   constructor(private httpClient : HttpClient) { }
+
+ 
   getMercado() {
     return this.httpClient.get(this.API).pipe(take(1));
   }
+
+  getMercadoDnD() {
+    return this.httpClient.get(this.APIMercado).pipe(take(1));
+  }
+
+  getCaracteristicasEquipamentos(){
+    this.getMercadoDnD().subscribe((data: any) => {
+      const equipamentos = data.results;
+      const caracteristicas: any[] = [];
+
+      equipamentos.forEach((equipamento: any) => {
+        this.httpClient.get(`https://www.dnd5eapi.co${equipamento.url}`).pipe(take(1)).subscribe((detalhes: any) => {
+          caracteristicas.push(detalhes);
+          if (caracteristicas.length === equipamentos.length) {
+            console.log(JSON.stringify(caracteristicas));
+          }
+        });
+      });
+    });
+  }
+
+
 
 
 }
